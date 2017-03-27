2615e4e4f421139758095bc9b7a7b95c5b6a5291
Tongcheng Li <tl486@nikola-compute01.coecis.cornell.edu>
2017-03-26 21:22:30 -0400

    dual mode

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 set -e
 
-TOOLS=/home/tl486/git_TCcaffe/caffeDropout/caffe/build/tools
+TOOLS=/home/tl486/git_TCcaffe/caffe_multiGPU/caffe/build/tools
 
-LD_PRELOAD="/opt/OpenBLAS/lib/libopenblas.so.0" $TOOLS/caffe train \
-  --solver=solver.prototxt
+LD_PRELOAD="/opt/OpenBLAS/lib/libopenblas.so.0 /usr/local/cuda-8.0/lib64/libcudart.so.8.0" $TOOLS/caffe train \
+  --solver=solver.prototxt --gpu=2,3
